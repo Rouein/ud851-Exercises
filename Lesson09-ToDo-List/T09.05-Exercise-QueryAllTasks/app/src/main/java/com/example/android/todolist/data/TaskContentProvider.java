4b1c11a6b8f276c04d7954fc52ec71d74c261193
rouein <rouein.fani@gmail.com>
2018-01-04 19:55:16 +0300

    Done

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -120,15 +120,49 @@ public class TaskContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
-        // TODO (1) Get access to underlying database (read-only for query)
+        // Done (1) Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
 
-        // TODO (2) Write URI match code and set a variable to return a Cursor
+        // Done (2) Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
 
-        // TODO (3) Query for the tasks directory and write a default case
+        Cursor retCursor;
 
-        // TODO (4) Set a notification URI on the Cursor and return that Cursor
+        // Done (3) Query for the tasks directory and write a default case
+        switch (match){
+            case TASKS:
+                retCursor = db.query(TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case TASK_WITH_ID:
+                //using selection and selectionargs
+                String id = uri.getPathSegments().get(1);
+                String mSelection = "_id=?";
+                String[] mSelectionArgs = new String[]{id};
 
-        throw new UnsupportedOperationException("Not yet implemented");
+                retCursor = db.query(TABLE_NAME,
+                        projection,
+                        mSelection,
+                        mSelectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+
+        }
+
+
+        // Done (4) Set a notification URI on the Cursor and return that Cursor
+        retCursor.setNotificationUri(getContext().getContentResolver(),uri);
+
+        return retCursor;
     }
 
 
